b6316b8c08762bdc46f856a5b571f8c5433367af
Rigney <krigney@proforma.com>
2018-04-30 16:29:06 -0400

    Angular routes working

--- GroupNews/Scripts/app.js
+++ GroupNews/Scripts/app.js
@@ -7,42 +7,42 @@ app.config(function ($routeProvider) {
         controller: "mainNewsController"
     })
 
-    $routeProvider.when("news/stories/:storyID", {
+    $routeProvider.when("/news/stories/:storyID", {
         templateUrl: "/Scripts/app/views/StoryPage.html",
         controller: "storyPageController"
     })
 
-    $routeProvider.when("news/stories/create-story", {
+    $routeProvider.when("/news/create-story", {
         templateUrl: "/Scripts/app/views/CreateStory.html",
         controller: "createStoryController"
     })
 
-    $routeProvider.otherwise({ redirectTo: "/news-dashboard" });
+    $routeProvider.otherwise({ redirectTo: "/news/stories" });
 })
 
 app.controller("mainNewsController", ["$scope", "$http", function ($scope, $http) {
 
-    const getEvents = () => {
-        let searchURL = "/api/news";
-        if ($scope.searchQuery) {
-            searchURL = searchURL + "?title=" + $scope.searchQuery
-        }
-        $http({
-            method: "GET",
-            url: searchURL
-        }).then(resp => {
-            console.log(resp.data);
-            $scope.events = resp.data;
-        })
-    }
+    //const getEvents = () => {
+    //    let searchURL = "/api/news";
+    //    if ($scope.searchQuery) {
+    //        searchURL = searchURL + "?title=" + $scope.searchQuery
+    //    }
+    //    $http({
+    //        method: "GET",
+    //        url: searchURL
+    //    }).then(resp => {
+    //        console.log(resp.data);
+    //        $scope.events = resp.data;
+    //    })
+    //}
 
-    $scope.searchForEvent = () => {
-        getEvents();
-    }
+    //$scope.searchForEvent = () => {
+    //    getEvents();
+    //}
 
     var start = () => {
-        $scope.pageTitle = "PROburg Events Page"
-        getEvents();
+        $scope.pageTitle = "testing";
+        //getEvents();
     }
 
     start();
